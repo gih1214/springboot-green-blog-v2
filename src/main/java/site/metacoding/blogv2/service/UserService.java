026a82036e9377e64a734aee5b1f6f01e6c0554d
gih1214 <gih1214@hanmail.net>
2022-03-22 12:35:26 +0900

    5. 회원가입 완료

--- src/main/java/site/metacoding/blogv2/service/UserService.java
+++ src/main/java/site/metacoding/blogv2/service/UserService.java
@@ -1,9 +1,11 @@
 package site.metacoding.blogv2.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.user.UserRepository;
+import site.metacoding.blogv2.web.api.dto.user.JoinDto;
 
 //  웹브라우저 -> 컨트롤러 -> 서비스 -> 레파지토리 -> 영속성컨텍스트 -> DB
 
@@ -12,5 +14,11 @@ import site.metacoding.blogv2.domain.user.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public void 회원가입(JoinDto joinDto) {
+        // save하면 db에 insert하고 insert된 결과를 다시 return 해준다.
+        userRepository.save(joinDto.toEntity());
+    }
 
 }
