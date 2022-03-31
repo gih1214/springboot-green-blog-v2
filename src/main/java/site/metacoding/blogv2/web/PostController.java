01d722fa60c7588a71b94f27c7720085daf9e151
gih1214 <gih1214@hanmail.net>
2022-04-01 03:00:09 +0900

    16. 글상세보기 완료

--- src/main/java/site/metacoding/blogv2/web/PostController.java
+++ src/main/java/site/metacoding/blogv2/web/PostController.java
@@ -1,9 +1,13 @@
 package site.metacoding.blogv2.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.post.Post;
+import site.metacoding.blogv2.service.PostService;
 
 // Controller 는 파일 리턴 전용 컨트롤러이다.
 // 브라우저를 위한 컨트롤러 (html 파일로 페이지 주는 컨트롤러)
@@ -11,6 +15,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Controller
 public class PostController {
+
+    private final PostService postService;
+
+    // 게시글 상세보기 페이지
+    @GetMapping("/post/{id}")
+    public String detail(@PathVariable Integer id, Model model) {
+        Post postEntity = postService.글상세보기(id);
+        model.addAttribute("post", postEntity);
+        return "post/detail";
+    }
 
     // 페이지를 줘
     // /s 붙었으니까 자동으로 인터셉터가 인증 체크함.
