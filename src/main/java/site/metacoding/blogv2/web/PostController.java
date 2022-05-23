dbccb832fccbf79093e9ccb7d36c2e68b610ae48
gih1214 <gih1214@hanmail.net>
2022-05-23 19:57:49 +0900

    19. 댓글 리스트 보기 완료

--- src/main/java/site/metacoding/blogv2/web/PostController.java
+++ src/main/java/site/metacoding/blogv2/web/PostController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.post.Post;
+import site.metacoding.blogv2.service.PostService;
 
 // Controller 는 파일 리턴 전용 컨트롤러이다.
 // 브라우저를 위한 컨트롤러 (html 파일로 페이지 주는 컨트롤러)
@@ -14,12 +16,14 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class PostController {
 
+    private final PostService postService;
+
     // 게시글 상세보기 페이지
     @GetMapping("/post/{id}")
     public String detail(@PathVariable Integer id, Model model) {
 
-        // Post postEntity = postService.글상세보기(id);
-        // model.addAttribute("post", postEntity);
+        Post postEntity = postService.글상세보기(id);
+        model.addAttribute("comments", postEntity.getComments());
         model.addAttribute("postId", id);
 
         return "post/detail";
