761d3897f4f80ccd77913052ca04383f9db59008
gih1214 <gih1214@hanmail.net>
2022-05-23 20:07:45 +0900

    20. 댓글 삭제 구현 완료

--- src/main/java/site/metacoding/blogv2/web/PostController.java
+++ src/main/java/site/metacoding/blogv2/web/PostController.java
@@ -1,13 +1,21 @@
 package site.metacoding.blogv2.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.comment.Comment;
 import site.metacoding.blogv2.domain.post.Post;
+import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.PostService;
+import site.metacoding.blogv2.web.api.dto.comment.CommentResponseDto;
 
 // Controller 는 파일 리턴 전용 컨트롤러이다.
 // 브라우저를 위한 컨트롤러 (html 파일로 페이지 주는 컨트롤러)
@@ -17,13 +25,34 @@ import site.metacoding.blogv2.service.PostService;
 public class PostController {
 
     private final PostService postService;
+    private final HttpSession session;
 
     // 게시글 상세보기 페이지
     @GetMapping("/post/{id}")
     public String detail(@PathVariable Integer id, Model model) {
 
         Post postEntity = postService.글상세보기(id);
-        model.addAttribute("comments", postEntity.getComments());
+        User principal = (User) session.getAttribute("principal");
+
+        List<CommentResponseDto> comments = new ArrayList<>();
+
+        for (Comment comment : postEntity.getComments()) {
+            CommentResponseDto dto = new CommentResponseDto();
+            dto.setComment(comment);
+
+            if (principal != null) {
+                if (principal.getId() == comment.getUser().getId()) {
+                    dto.setAuth(true); // or false
+                } else {
+                    dto.setAuth(false); // or false
+                }
+            } else {
+                dto.setAuth(false); // or false
+            }
+            comments.add(dto);
+        }
+
+        model.addAttribute("comments", comments);
         model.addAttribute("postId", id);
 
         return "post/detail";
